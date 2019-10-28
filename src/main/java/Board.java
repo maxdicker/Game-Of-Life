6fe6a4772fba10603568400e61e462b24e00a09e
max.dicker <max.dicker@myob.com>
2019-10-28 17:40:17 +1100

    Add getNeighbours() for Board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -8,21 +8,38 @@ public class Board {
     private int width;
     private int height;
 
-    public Board(int width, int height, List<Coordinates> positionsOfAliveCells) {
+    public Board(int width, int height, List<Coordinates> positionsOfLivingCells) {
         this.width = width;
         this.height = height;
 
         this.neighboursByCell = new LinkedHashMap<>();
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                neighboursByCell.put(new Cell(false), new Cell[8]);
+        for (int i = 0; i < height * width; i++) {
+            neighboursByCell.put(new Cell(false), new Cell[0]);
+        }
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Coordinates position = new Coordinates(x, y);
+                neighboursByCell.put(getCell(position), findNeighbours(position));
             }
         }
 
-        for (Coordinates position : positionsOfAliveCells) {
+        for (Coordinates position : positionsOfLivingCells) {
             getCell(position).setState(true);
         }
+    }
+
+    private Cell[] findNeighbours(Coordinates position) {
+        int up = (position.y + height - 1) % (height);
+        int down = (position.y + height + 1) % (height);
+        int left = (position.x + width - 1) % (width);
+        int right = (position.x + width + 1) % (width);
+
+        return new Cell[] {getCell(new Coordinates(left, up)), getCell(new Coordinates(position.x, up)),
+                getCell(new Coordinates(right, up)), getCell(new Coordinates(right, position.y)), getCell(new Coordinates(right, down)),
+                getCell(new Coordinates(position.x, down)), getCell(new Coordinates(left, down)), getCell(new Coordinates(left, position.y))
+        };
     }
 
     public Set<Cell> getCells() {
@@ -32,6 +49,10 @@ public class Board {
     public Cell getCell(Coordinates position) {
         int positionInSet = position.y * width + position.x;
         return (Cell) neighboursByCell.keySet().toArray()[positionInSet];
+    }
+
+    public Cell[] getNeighbours(Cell cell) {
+        return neighboursByCell.get(cell);
     }
 
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,13 +19,37 @@ public class BoardTest {
     }
 
     @Test
-    public void newBoardCreatesLiveCells() {
+    public void newBoardInstantiatesLivingCellAtSpecifiedPosition() {
         int boardWidth = 5;
         int boardHeight = 5;
-        Coordinates positionOfAliveCell = new Coordinates(4, 4);
-        Board board = new Board(boardWidth, boardHeight, Collections.singletonList(positionOfAliveCell));
+        Coordinates positionOfLiveCell = new Coordinates(4, 4);
+        Board board = new Board(boardWidth, boardHeight, Collections.singletonList(positionOfLiveCell));
 
-        Cell aliveCell = board.getCell(positionOfAliveCell);
+        Cell aliveCell = board.getCell(positionOfLiveCell);
         assertTrue(aliveCell.isAlive());
+    }
+
+    @Test
+    public void boardCorrectlyIdentifiesNeighbours() {
+        int boardWidth = 5;
+        int boardHeight = 5;
+        Coordinates referenceCellPosition = new Coordinates(0, 0);
+        List<Coordinates> positionsOfNeighbours = Arrays.asList(new Coordinates(1, 0), new Coordinates(1, 1),
+                                                        new Coordinates(0, 1), new Coordinates(4, 1),
+                                                        new Coordinates(4, 0), new Coordinates(4, 4),
+                                                        new Coordinates(0, 4), new Coordinates(1, 4));
+        Board board = new Board(boardWidth, boardHeight, positionsOfNeighbours);
+
+        Cell referenceCell = board.getCell(referenceCellPosition);
+
+        boolean neighboursAllAlive = true;
+        for (Cell cell : board.getNeighbours(referenceCell)) {
+            if (!cell.isAlive()) {
+                neighboursAllAlive = false;
+                break;
+            }
+        }
+
+        assertTrue(neighboursAllAlive);
     }
 }
