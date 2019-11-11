174ee7ac01a85ac59d16a11691f8cf4494f7d070
max.dicker <max.dicker@myob.com>
2019-11-11 17:25:23 +1100

    Add tests for Validator

---
+++ src/test/java/UtilsTest.java
@@ -0,0 +1,39 @@
+import core.Coordinates;
+import org.junit.Test;
+import utils.DataValidator;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class UtilsTest {
+    private int numMin = 1;
+    private int numMax = 3;
+    private int numWithinRange = 1;
+    private int numOutsideRange = 4;
+    private Coordinates positionMin = new Coordinates(0,0);
+    private Coordinates positionMax = new Coordinates(3,3);
+    private List<Coordinates> positionsWithinRange = Arrays.asList(new Coordinates(3,3), new Coordinates(0,3));
+    private List<Coordinates> positionsOutsideRange = Arrays.asList(new Coordinates(3,3), new Coordinates(4,3));
+
+    @Test
+    public void Given_NumberWithinRange_When_Validating_Then_ValidatorReturnsTrue() {
+        assertTrue(DataValidator.numberInRange(numWithinRange, numMin, numMax));
+    }
+
+    @Test
+    public void Given_NumberOutsideRange_When_Validating_Then_ValidatorReturnsFalse() {
+        assertFalse(DataValidator.numberInRange(numOutsideRange, numMin, numMax));
+    }
+
+    @Test
+    public void Given_APositionWithinRange_When_Validating_Then_ValidatorReturnsTrue() {
+        assertTrue(DataValidator.positionsInRange(positionsWithinRange, positionMin, positionMax));
+    }
+
+    @Test
+    public void Given_APositionOutsideRange_When_Validating_Then_ValidatorReturnsFalse() {
+        assertFalse(DataValidator.positionsInRange(positionsOutsideRange, positionMin, positionMax));
+    }
+}
